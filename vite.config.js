89c2a115084e16932f935399a50b75da19052848
Dhruw <dharmendra23101@iiitnr.edu.in>
2025-08-07 13:53:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Use this if deploying to https://dharmendra23101.github.io/Portfolio/
+ 
 })
