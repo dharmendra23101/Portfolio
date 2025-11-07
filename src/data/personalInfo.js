5f56baf673c0f0f66b3bac090a496662c6a31ac1
Dhruw <dharmendra23101@iiitnr.edu.in>
2025-11-08 00:19:15 +0530

    Update personalInfo.js

--- src/data/personalInfo.js
+++ src/data/personalInfo.js
@@ -6,10 +6,11 @@ const personalInfo = {
   location: "Naya Raipur, Chhattisgarh, India",
   bio: "I'm a passionate Full Stack Developer and Competitive Programmer with experience in building responsive web applications, solving complex algorithms, and implementing innovative solutions for real-world problems.",
   github: "https://github.com/dharmendra23101",
-  linkedin: "linkedin.com/in/dharmendra-dhruw-7b3569391/",
+  linkedin: "https://www.linkedin.com/in/dharmendra-dhruw-7b3569391/",
   twitter: "https://twitter.com/",
   instagram: "https://instagram.com/"
 };
 
 
 export { personalInfo };
+
